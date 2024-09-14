28f92d0d5310c572488315f5bffe25230be8af18
chase <chasejhorton@gmail.com>
2024-09-13 20:04:04 -0500

    added link to map on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <div className="flex items-center justify-center w-screen h-screen">
-    <Button variant="outline" className="" asChild>
-      <a href="/login">Login</a>
-    </Button>
+      <div className="flex flex-col gap-y-4">
+      <Button variant="outline" className="" asChild>
+        <a href="/login">Login</a>
+      </Button>
+      <Button variant="outline" className="" asChild>
+        <a href="/map">FBI Crime Data Graphing Tool</a>
+      </Button>
+      </div>
    </div>
   );
 }
